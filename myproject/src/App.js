ffdf426c821cb1f6cc49f8bde028aeb6d6a24945
PacJ <pjw9267@gmail.com>
2023-03-27 17:36:27 +0900

    2023-03-27

---
+++ myproject/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyJsx001 from './components/exo1_jsx/MyJsx001';
+import MyJsx002 from './components/exo1_jsx/MyJsx002';
+import MyJsx003 from './components/exo1_jsx/MyJsx003';
+function App() {
+  return (
+    <div className='App'>
+      {/* <MyJsx001></MyJsx001> */}
+      {/* <MyJsx002></MyJsx002> */}
+      <MyJsx003></MyJsx003>
+    </div>
+  );
+}
+
+export default App;
